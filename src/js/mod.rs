fd44d2896691a2d43ed7b17ab1a6a296836703c5
Jan Kaul <jankaul@mailbox.org>
2022-02-28 13:18:28 +0100

    remove bundled dependencies

--- src/js/mod.rs
+++ src/js/mod.rs
@@ -1,23 +1,16 @@
 use js_sys::Error;
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen(module = "/src/js/includes.js")]
+#[wasm_bindgen(
+    inline_js = "export function importjs(str){return import(/* webpackIgnore: true */str)}"
+)]
 extern "C" {
     #[wasm_bindgen(catch, js_name=importjs)]
     pub async fn import_js(input: &str) -> Result<JsValue, JsValue>;
-    #[wasm_bindgen(catch, js_name=importDanfoJS)]
-    pub async fn import_danfojs() -> Result<JsValue, JsValue>;
-    #[wasm_bindgen(catch, js_name=importTFJS_VIS)]
-    pub async fn import_tfjs_vis() -> Result<JsValue, JsValue>;
 }
 
 pub async fn import(input: &str) -> Result<JsValue, Error> {
-    if input == "@tensorflow/tfjs-vis" {
-        import_tfjs_vis().await.map_err(|x| Error::from(x))
-    } else if input == "danfojs" {
-        import_danfojs().await.map_err(|x| Error::from(x))
-    } else if input.starts_with("https://cdn.skypack.dev")
-        || input.starts_with("https://cdn.jsdelivr.net")
+    if input.starts_with("https://cdn.skypack.dev") || input.starts_with("https://cdn.jsdelivr.net")
     {
         import_js(input).await.map_err(|x| Error::from(x))
     } else {
